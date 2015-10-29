0d736b6f425c187338e6500c7cdec4c060f9ddf2
Casiano Rodriguez Leon <casiano.rodriguez.leon@gmail.com>
2015-10-29 16:13:09 +0000

    added gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,9 @@
+  var gulp, shell;
+
+  gulp = require('gulp');
+
+  shell = require('gulp-shell');
+
+  gulp.task('default', function() {
+    return gulp.src('').pipe(shell('DEBUG=coffeepress:* nodemon bin/www'));
+  });
